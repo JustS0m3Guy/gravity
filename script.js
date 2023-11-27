3c163d6abf2ce32d338fba5a46fea5c35a20cf89
balu <balazs200428@gmail.com>
2023-11-27 11:04:27 +0100

    reset work in progress

--- script.js
+++ script.js
@@ -5,25 +5,35 @@ stopbtn.addEventListener("click", animationStop);
 let globalID;
 let running = false;
 
-let sun = new Celestialb("Sun", 100, new Vector(0, 0), new Vector(1, 1), "#FF0000", "#000000");
-let earth = new Celestialb("Earth", 10, new Vector(-10, 30), new Vector(-1, 1), "#0000FF", "#000000");
+let sun = new Celestialb("Sun", 500, new Vector(0, 0), new Vector(1, 1), "#FF0000", "#000000");
+let earth = new Celestialb("Earth", 100, new Vector(-40, 35), new Vector(-1, 1), "#0000FF", "#000000");
+let moon = new Celestialb("Moon", 10, new Vector(-30, 50), new Vector(-1, 1), "#00FF00", "#000000");
 
-let celestialbs = [sun, earth];
+let latest_celestial_startpoz = [][Vector];
+let celestialbs = [sun, earth, moon];
 
 function initialisation(){
     canvas.appendChild(sun.svgobject);
     canvas.appendChild(earth.svgobject);
+    canvas.appendChild(moon.svgobject);
+    for (let i = 0; i < celestialbs.length; i++) {
+        latest_celestial_startpoz.push(celestialbs[i].name);
+        latest_celestial_startpoz[i][0] = celestialbs[i].p;
+        latest_celestial_startpoz[i][1] = celestialbs[i].v;
+    }
+    console.log(latest_celestial_startpoz);
 }
 
 function simulation_step(){
     Celestialb.gravitational_effect(celestialbs);
     for (const celestialbody of celestialbs) {
         celestialbody.move();
-    }    
+    }
+    //console.log(sun, earth);
 }
 
 function reset(){
-    
+
 }
 
 //--------------------------------------------------
